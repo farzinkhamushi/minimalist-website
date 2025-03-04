2fac99b44a082590a3f14e137f2c29c4a605430d
Programming party <kuroshkhamushi@gmail.com>
2025-03-04 16:12:30 +0330

    finalized and justified the project

--- script.js
+++ script.js
@@ -21,23 +21,30 @@ burger.addEventListener('click',()=>{
 burger.addEventListener('click',()=> {
     if (burger.classList.contains('active')) {
         gsap.to(".links",{x: "100%"});
-        gsap.to(".line",{stroke: "white",x:0});
+        //
+        gsap.to(".line",{stroke: "white",scaleX:1});
+        ///////////////////////////////////////
         gsap.set("body",{overflow: "auto"});
-        gsap.set("body",{overflowX: "auto"});
-        gsap.to(burger,{borderWidth:3 , borderRadius:"0%" ,scaleX: 1});        
+        gsap.set("body",{overflowX: "hidden"});
+        //gsap.to(burger,{borderWidth:3 , borderRadius:"0%" ,scaleX: 1});        
     }else{
         gsap.to(".links",{x: "0%"});
-        gsap.to(".line",{stroke: "black",x:5});
+        //
+        gsap.to(".line",{stroke: "black",scaleX:0.5});
+        //gsap.to(burger,{backgroundColor:"black"});
+        //////////////////////////////////////////
         gsap.fromTo('.links a',
             { opacity:0, y: 0 },
             { opacity:1, y: 20 ,delay:0.25,stagger:0.25 })
             gsap.set("body",{overflow: "hidden"});
-        gsap.to(burger ,{borderWidth:0, borderRadius:"50%", scaleX:0.5});
+        //gsap.to(burger ,{borderWidth:0, borderRadius:"50%", scaleX:0.5});
     }
     burger.classList.toggle("active");
 })
 
-const videos = gsap.utils.toArray(" .video ");
+
+
+const videos = gsap.utils.toArray('.video');
 gsap.set( videos ,{ opacity : 0 } );
 
 videos.forEach((video) => {
@@ -45,10 +52,13 @@ videos.forEach((video) => {
         trigger: video,
         start: "top center",
         end: "bottom center",
-        markers: true,
+        markers: false,
         onEnter: () => {
             gsap.to(video, {opacity:1});
             video.play();
         },
+        onEnterBack: () => video.play(),
+        onLeave: () => video.pause(),
+        onLeaveBack: () => video.pause(),
     });
 });
